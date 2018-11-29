18a5a59af24df14e7881ce2403014dfd0a5668fe
songminju <alswn1531@naver.com>
2018-11-29 21:28:47 +0900

    Counsel information

---
+++ team.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<string.h>
+#include<curses.h>
+
+int main() 
+{
+	int i, j;
+	char barcode[20];
+	scanf("%s", &barcode);
+	
+	initscr();
+
+	clear();
+	move(10,20);
+	addstr("***********Counsel Content************");
+	move(13,20);
+	if (strcmp(barcode, "2017113151") == 0) {
+		addstr("[Student Information]");
+		move(15,20);
+		addstr("Song Min Ju  2017113151");
+		move(17,20);
+		addstr("Professor : Han Gi Jun");
+		move(19,20);
+		addstr("Counsel count : 1");
+		move(21,20);
+		addstr("Counsel date : 2018.05.01");
+		move(23,0);
+	}
+	else if (strcmp(barcode, "2017113135") == 0) {
+                addstr("[Student Information]");
+                move(15,20);
+                addstr("Jung Ji U  2017113135");
+                move(17,20);
+                addstr("Professor : Lee Eun Ju");
+                move(19,20);
+                addstr("Counsel count : 2");
+                move(21,20);
+                addstr("Counsel date : 2017.04.22");
+                move(23,20);
+		addstr("         : 2018.11.22");
+		move(25,0);
+	      }
+	 else if (strcmp(barcode, "2017113231") == 0) {
+                addstr("[Student Information]");
+                move(15,20);
+                addstr("Kim Hyun Woo  2015113231");
+                move(17,20);
+                addstr("Professor : Tak Byeong Cheol");
+                move(19,20);
+                addstr("Counsel count : 1");
+                move(21,20);
+                addstr("Counsel date : 2018.10.13");
+                move(23,0);
+              }
+	 else {
+                addstr("[Student Information]");
+                move(15,20);
+                addstr("Soek Chan hui  2015111523");
+                move(17,20);
+                addstr("Professor : Hong Seok in");
+                move(19,20);
+                addstr("Counsel count : 2");
+                move(21,20);
+                addstr("Counsel date : 2016.10.01  2018.09.27");
+		move(23,0);
+
+              }
+	
+	refresh();
+	getch();
+
+	endwin();
+}
+		
+
